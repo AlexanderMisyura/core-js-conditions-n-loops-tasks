596a604e36f34c6faf3c0d9f2be4d14d87359fd7
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2024-01-29 15:18:50 +0300

    feat: add shuffleChar task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -477,8 +477,31 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let rounds = iterations;
+  let result = str;
+
+  while (rounds) {
+    let left = '';
+    let right = '';
+
+    for (let i = 0; i < result.length; i += 1) {
+      if (i % 2 !== 0) {
+        right += result[i];
+      } else {
+        left += result[i];
+      }
+    }
+
+    result = `${left}${right}`;
+    rounds -= 1;
+
+    if (result === str) {
+      rounds = iterations % (iterations - rounds);
+    }
+  }
+
+  return result;
 }
 
 /**
